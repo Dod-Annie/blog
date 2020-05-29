e219476d0fdc7c16e9c45f44d6ab92c6852b3fa6
liuzan <liuzan@dipont.com>
2020-05-29 17:36:27 +0800

    init

---
+++ src/main/java/com/dodannie/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package com.dodannie.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ *  @author  Dod_Annie
+ * @date */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dodannie/blog/enums/ResultEnum.java
@@ -0,0 +1,13 @@
+package com.dodannie.blog.enums;
+
+/**
+ * @author Dod_Annie
+ * @description
+ * @date 2020/5/27 19:25
+ */
+public enum ResultEnum {
+    /**
+     * 返回结果枚举
+     */
+
+}
