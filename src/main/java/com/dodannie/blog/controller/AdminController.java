49c15cb8377c810bca200ff3df4b991dee653531
Dod-Annie <936166900@qq.com>
2020-06-23 22:57:20 +0800

    update

--- src/main/java/com/dodannie/blog/controller/AdminController.java
+++ src/main/java/com/dodannie/blog/controller/AdminController.java
@@ -1,8 +1,8 @@
 package com.dodannie.blog.controller;
 
 import com.dodannie.blog.enums.ResultEnum;
-import com.dodannie.blog.enums.StateEnums;
 import com.dodannie.blog.pojo.Admin;
+import com.dodannie.blog.service.AdminService;
 import com.dodannie.blog.token.UsernamePasswordToken;
 import com.dodannie.blog.utils.Result;
 import com.dodannie.blog.utils.ShiroUtils;
@@ -10,6 +10,7 @@ import com.dodannie.blog.utils.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +28,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Result login(@RequestBody Admin admin) {
@@ -58,6 +63,37 @@ public class AdminController {
         Admin loginAdmin = (Admin) ShiroUtils.getLoginUser();
         loginAdmin.setPassword("");
         return new Result<>(loginAdmin);
+    }
+
+    /**
+     * 查询管理员
+     */
+    @RequestMapping(value = "/getAdmin", method = RequestMethod.GET)
+    public Result<Admin> getAdmin() {
+        Admin admin = adminService.getAdmin();
+        return new Result<>(admin);
+    }
+
+    /**
+     * 更新个人信息
+     * @param admin
+     * @return
+     */
+    @RequestMapping(value = "/updateInfo", method = RequestMethod.PUT)
+    public Result<Object> updateInfo(@RequestBody Admin admin) {
+        adminService.updateInfo(admin);
+        return new Result<>("更新成功！");
+    }
+
+    /**
+     * 更新密码
+     * @param admin
+     * @return
+     */
+    @RequestMapping(value = "/updatePassword", method = RequestMethod.PUT)
+    public Result<Object> updatePassword(@RequestBody Admin admin) {
+        adminService.updatePassword(admin);
+        return new Result<>("更新成功！");
     }
 
 }
